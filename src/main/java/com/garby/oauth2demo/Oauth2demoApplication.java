a7e4afba87be5ad1d10bf8c58f509c0b16ef4062
Gabriel Madichie <amadichie@yahoo.com>
2023-02-12 14:49:24 +0100

    Initial commit

---
+++ src/main/java/com/garby/oauth2demo/Oauth2demoApplication.java
@@ -0,0 +1,13 @@
+package com.garby.oauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garby/oauth2demo/Oauth2demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garby.oauth2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
