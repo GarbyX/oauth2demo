7cb0986596229824ab25596153d700f59de3edcc
Gabriel Madichie <amadichie@yahoo.com>
2023-02-12 15:41:59 +0100

    2nd commit | Client ids | SecurityConfig - added Client ids for Google & Facebook  (they enable Oauth2ClientAutoConfiguration class, which sets up all the necessary beans) - created SecurityConfig.java (The automatic web security configuration is equivalent to defining a simple oauth2Login() element)

---
+++ src/main/java/com/garby/oauth2demo/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.garby.oauth2demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+}
